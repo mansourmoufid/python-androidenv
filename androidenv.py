d253401af75200e4b14a2e4a4d083cc852791a31
Mansour Moufid <mansourmoufid@gmail.com>
2021-12-01 10:35:52 -0500

    Forgot to bump this up.

--- androidenv.py
+++ androidenv.py
@@ -10,7 +10,7 @@ import sys
 __author__ = 'Mansour Moufid'
 __copyright__ = 'Copyright 2021, Mansour Moufid'
 __license__ = 'ISC'
-__version__ = '0.1'
+__version__ = '0.2.1'
 __email__ = 'mansourmoufid@gmail.com'
 __status__ = 'Development'
 
