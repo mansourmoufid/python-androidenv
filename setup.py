475e3cc4774d752c9c7a4099698845d18a4255be
Mansour Moufid <mansourmoufid@gmail.com>
2021-12-26 13:01:57 -0500

    Move metadata to setup.cfg.

--- setup.py
+++ setup.py
@@ -1,40 +1,10 @@
 #!/usr/bin/env python
 
-import androidenv
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-
-_classifiers = [
-    'Development Status :: 4 - Beta',
-    'Environment :: Console',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: ISC License (ISCL)',
-    'Operating System :: Android',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Topic :: Software Development :: Build Tools',
-]
-
-with open('README.md', 'r') as readme:
-    _long_description = readme.read()
-
-
-if __name__ == '__main__':
-
-    try:
-        from setuptools import setup
-    except ImportError:
-        from distutils.core import setup
-
-    setup(
-        author=androidenv.__author__,
-        author_email=androidenv.__email__,
-        classifiers=_classifiers,
-        description=androidenv.__doc__,
-        license=androidenv.__license__,
-        long_description=_long_description,
-        long_description_content_type='text/markdown',
-        name='androidenv',
-        py_modules=['androidenv'],
-        url='https://github.com/eliteraspberries/python-androidenv',
-        version=androidenv.__version__,
-    )
+setup(
+    py_modules=['androidenv'],
+)
